c36a19ff079d00625b74d9cde0571e3a5eb944fc
Itzumyk <61480642+Itzumyk@users.noreply.github.com>
2020-08-06 22:43:53 +0300

    Update PHISHERxPROxBEST.sh

--- PHISHERxPROxBEST.sh
+++ PHISHERxPROxBEST.sh
@@ -7,3 +7,21 @@ command -v unzip > /dev/null 2>&1 || { echo >&2 "Unzip is not installed ! Instal
 }
 banner() {
 clear
+printf " \e[33;1m o--o  o  o o-O-o  o-o  o  o o--o o--o      o--o  o--o   o-o     o----o o----   o-----o        \e[0m\n"
+printf " \e[33;1m |   | |  |   |   |     |  | |    |   |     |   | |   | o  /o    |    | |          |     \e[0m\n"
+printf " \e[33;1m O--o  O--O   |    o-o  O--O O-o  O-Oo  \ / O--o  O-Oo  | / | \ /|----| |----      |         \e[0m\n"
+printf " \e[33;1m |     |  |   |       | |  | |    |  \   o  |     |  \  o/  o  o |    | |          |      \e[0m\n"
+printf " \e[33;1m o     o  o o-O-o o--o  o  o o--o o   o / \ o     o   o  o-o  / \o----o |---- S    |                \e[0m\n"
+printf " \n"
+printf " \e[1;92m v1.0, Author: Itzu Myk                                                                      \e[0m\n"
+printf " \n"
+printf " \e[1;31m ×=========================================================================================×                           \e[0m\n"
+printf " \e[1;31m ×         Developer - Itzu Myk                                                            ×                            \e[0m\n"
+printf " \e[1;31m ×      Instagram - @itzumyk (https://instagram.com/itzumyk?igshid=i6r4swwfb99c)           ×                             \e[0m\n"
+printf " \e[1;31m ×         YouTube - Itzu Myk (https://www.youtube.com/channel/UCEbvRFT5j6ksLf2TIypwr0A)   ×                              \e[0m\n"
+printf " \e[1;31m ×=========================================================================================×                               \e[0m\n"
+printf " \n"
+printf " \e[36;1m.:.Select Any Attack for your Victim.:.\e[0m\n"
+printf " \n"
+}
+menu() {
